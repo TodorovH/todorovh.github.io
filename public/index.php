6ec913c7db558c824a9579576eca58654f412ac0
hristo todorov <hristo_petkov2001@abv.bg>
2018-10-20 00:34:49 +0300

    msg

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NFA</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div id="main-container">
+		<div id="logo-container">
+			<img src="img/nfa_gold_logo.png"><br>
+			<p>NATIONAL FOOTBALL ACADEMY</p>
+		</div>
+		<div id="right-text">
+			<p>WE WANT YOU!</p>
+			<span>3 DAY CAMP</span>
+		</div>
+		<div id="list-container">
+			<ul>
+				<li>QUARTERBACKS</li>
+				<li>RECEIVERS</li>
+				<li>RUNNING BACKS</li>
+			</ul>
+		</div>
+		<div id="button-outer-container">
+			<div id="button-inner-container">
+				<button id="register" type="button" onclick="location.href='https://campscui.active.com/orgs/NationalFootballAcademies#/selectSessions/2493501/filter/session=40045601'">Register</button>
+				<p>December 2018 - April 2019</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
